0af75fc0f26ab30140ffa28fa8b7b607cf926298
Wilhson <Wilhomega@hotmail.com>
2018-09-08 16:30:23 +0200

    defe
    ef

---
+++ constructor.py
@@ -0,0 +1,42 @@
+import json
+import os
+
+class constructor:
+    # Create the file config
+    def config_txt():
+        config = {}
+        data = json.dumps(config)
+        with open("Alterable\Config.txt", "w") as file:
+            file.write(data)
+    # Create the file player
+    def player_txt():
+        player = {}
+        player["player"] = {"position": "0"}
+        player["inventory"] = {"slot number": "4"}
+        data = json.dumps(player)
+        with open("Alterable\Player.txt", "w") as player:
+            player.write(data)
+    # Create the file monders
+    def monders_txt():
+        monders = {}
+        monders["johnson"] = {"slots": "6","endurance": "6","strength": "2","vision": "2"}
+        data  = json.dumps(monders)
+        with open("Alterable\Monders.txt", "w") as monders:
+            monders.write(data)
+    # Creation du fichier options
+    def options_txt():
+        data = {"options": {"width": "1920", "height": "1080", "volume": "0.2", "character": "0"}}
+        data = json.dumps(data)
+        with open("Alterable\Options.txt", "w") as options:
+            options.write(data)
+
+if not os.path.exists(os.curdir+"\Alterable"):
+    os.mkdir("Alterable")
+if not os.path.exists(os.curdir+"Alterable\Monders.txt"):
+    constructor.monders_txt()
+if not os.path.exists(os.curdir+"Alterable\Player.txt"):
+    constructor.player_txt()
+if not os.path.exists(os.curdir+"Alterable\Config.txt"):
+    constructor.config_txt()
+if not os.path.exists(os.curdir+"Alterable\Options.txt"):
+    constructor.options_txt()
