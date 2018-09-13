953b627ed5bed59fb4cb61a1e9091c526e0b7baa
CHARVOLIN Guillaume <g.charvolin@mulatiere.lan>
2018-09-13 12:05:40 +0200

    Hehuhf

--- load.py
+++ load.py
@@ -5,5 +5,6 @@ from tools import *
 
 spriteplayer = tools.separate("Assets\Image\SpritePlayer.png", 78)
 icon = pygame.transform.scale(pygame.image.load("Assets\Image\Icon.png").convert_alpha(), (32, 32))     #convert alpha use the transparence
-background = pygame.image.load("Assets\Image\Room.png").convert_alpha()
+background = tools.load_convert("Assets\Image\Room.png")
 background = pygame.transform.scale(background.convert_alpha(), (1920, 1080))
+textbox = tools.load_convert("Assets\Image\Textbox")
